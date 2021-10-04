b431e34e1f401b2c728c6328b7977e397d2cbaa8
Thiago Dantas Soares <thiagosoares93@hotmail.com>
2021-10-04 10:34:15 -0300

    Update 1186.py
    format

--- 1186.py
+++ 1186.py
@@ -4,10 +4,10 @@ soma = 0
 for i in range(12):
   for j in range(12):
     a = float(raw_input())
-    if ((i+j+1) > 12):
+    if ( ( i + j + 1 ) > 12):
       soma = soma+a
 
 if O=="S":
   print("%.1f" %soma)
 else:
-  print("%.1f" %(soma/66.0))
+  print("%.1f" %(soma / 66.0))
