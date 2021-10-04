d5214b233fb2f129acbbc73a5543b8c029bf2bdb
Thiago Dantas Soares <thiagosoares93@hotmail.com>
2021-10-04 10:33:35 -0300

    Update 1099.py
    format

--- 1099.py
+++ 1099.py
@@ -2,10 +2,10 @@ n = int(raw_input())
 
 soma= 0
 for i in range (n):
-  x, y = map(int,raw_input().split())
+  x, y = map(int, raw_input().split())
   if y > x:
     somax = 0
-    for i in range (x+1,y):
+    for i in range (x + 1, y):
       if i%2 ==1:
        somax += i
     print "%d" %(somax) 
@@ -13,7 +13,7 @@ for i in range (n):
   
   else:
     somay = 0
-    for i in range (y+1,x):
-      if i%2 ==1:
+    for i in range (y + 1, x):
+      if i % 2 == 1:
         somay += i
     print "%d" %(somay)
